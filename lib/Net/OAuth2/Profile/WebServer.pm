c18e81fcdad0da631e34b0a6ad4a64fdf0eb2326
niko <nikomomo@gmail.com>
2011-12-21 10:44:50 +0100

    get_access_token is now able to load a previously fetched token

--- lib/Net/OAuth2/Profile/WebServer.pm
+++ lib/Net/OAuth2/Profile/WebServer.pm
@@ -10,60 +10,67 @@ use HTTP::Request::Common;
 __PACKAGE__->mk_accessors(qw/redirect_uri grant_type/);
 
 sub authorize_params {
-  my $self = shift;
-  my %options = $self->SUPER::authorize_params(@_);
-  $options{response_type} = 'code';
-  $options{redirect_uri} = $self->redirect_uri if defined $self->redirect_uri;
-  # legacy for pre v2.09 (37Signals)
-  $options{type} = 'web_server';
-  return %options;
+	my $self = shift;
+	my %options = $self->SUPER::authorize_params(@_);
+	$options{response_type} = 'code';
+	$options{redirect_uri} = $self->redirect_uri if defined $self->redirect_uri;
+	# legacy for pre v2.09 (37Signals)
+	$options{type} = 'web_server';
+	return %options;
 }
 
 sub get_access_token {
-  my $self = shift;
-  my $code = shift;
-  my %req_params = @_;
+	my $self = shift;
+	my %prm  = @_;
+	my $dta  = {};
+	if( my $code = delete $prm{code} ) { #assume this is the first call
+		my $request;
+		if ($self->client->access_token_method eq 'POST') {
+			$request = POST($self->client->access_token_url(), {$self->access_token_params($code, %prm)});
+		} else {
+			$request = HTTP::Request->new(
+				$self->client->access_token_method => $self->client->access_token_url(
+					$self->access_token_params($code, %prm)
+				)
+			);
+		}
+		my $response = $self->client->request($request);
+		die "Fetch of access token failed: " . $response->status_line . ": " . $response->decoded_content
+			unless $response->is_success;
+		$dta = _parse_json($response->decoded_content);
 
-  my $request;
-  if ($self->client->access_token_method eq 'POST') {
-    $request = POST($self->client->access_token_url(), {$self->access_token_params($code, %req_params)});
-  } else {
-    $request = HTTP::Request->new(
-      $self->client->access_token_method => $self->client->access_token_url($self->access_token_params($code, %req_params))
-  );
-  }
-  my $response = $self->client->request($request);
-  die "Fetch of access token failed: " . $response->status_line . ": " . $response->decoded_content unless $response->is_success;
-  my $res_params = _parse_json($response->decoded_content);
-  $res_params = _parse_query_string($response->decoded_content) unless defined $res_params;
-  die "Unable to parse access token response '".substr($response->decoded_content, 0, 64)."'" unless defined $res_params;
-  $res_params->{client} = $self->client;
-  return Net::OAuth2::AccessToken->new(%$res_params);
+		#die "RESPONSE",$response->decoded_content;
+		$dta = _parse_query_string($response->decoded_content) unless defined $dta;
+		die "Unable to parse access token response '".substr($response->decoded_content, 0, 64)."'" unless defined $dta;
+	} else {
+		$dta = { %prm };
+	}
+	$dta->{client} = $self->client;
+	return Net::OAuth2::AccessToken->new(%$dta);
 }
 
 sub access_token_params {
-  my $self = shift;
-  my $code = shift;
-  my %options = $self->SUPER::access_token_params($code, @_);
-  $options{code} = $code;
-  $options{grant_type} = 'authorization_code';
-  $options{redirect_uri} = $self->redirect_uri if defined $self->redirect_uri;
-  # legacy for pre v2.09 (37Signals)
-  $options{type} = 'web_server';
-  return %options;
+	my $self = shift;
+	my $code = shift;
+	my %options = $self->SUPER::access_token_params($code, @_);
+	$options{code} = $code;
+	$options{grant_type} = 'authorization_code';
+	$options{redirect_uri} = $self->redirect_uri if defined $self->redirect_uri;
+	$options{type} = 'web_server';
+	return %options;
 }
 
 sub _parse_query_string {
-  my $str = shift;
-  my $uri = URI->new;
-  $uri->query($str);
-  return {$uri->query_form};
+	my $str = shift;
+	my $uri = URI->new;
+	$uri->query($str);
+	return {$uri->query_form};
 }
 
 sub _parse_json {
-  my $str = shift;
-  my $obj = eval{local $SIG{__DIE__}; decode_json($str)};
-  return $obj;
+	my $str = shift;
+	my $obj = eval{local $SIG{__DIE__}; decode_json($str)};
+	return $obj;
 }
 
 =head1 NAME
